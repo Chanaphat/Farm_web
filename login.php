ce83e85275b623ebb08a23c998845effaffcc0a3
Chanaphat <chanaphataim@gmail.com>
2017-09-21 17:13:00 +0700

    login.php

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<style>
+
+</style>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+
+<head>
+<title>Pig Thailand</title>
+<meta charset="iso-8859-1">
+
+<link rel="stylesheet" href="styles/layout.css" type="text/css">
+<!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
+
+</head>
+<body>
+       <div style="padding:100px; border: 200px solid #eeeeee; background-color: #FAFFF5;">
+           <form name="frmLogin" method="post" action="check_login.php" >
+		   <font size = 5>
+                                       ล็อกอินเข้าสู่ระบบ...<br></font>
+                  <table border="0" cellspacing="0" cellpadding="5" style="width: 300px">
+						<tr>
+							<td>ชื่อผู้ใช้งาน  :</td>
+							
+							<td>
+									<input name="username" type="text" id="username">
+							</td>
+						</tr>
+						<tr>
+							<td>รหัสผ่าน  :</td>
+							
+							<td>
+									<input name="password" type="password" id="password">
+							</td>
+						</tr>
+					<td>
+				<input type="submit" name="btnsummit" value="เข้าสู่ระบบ">
+				
+		    </td>
+			</tr>
+		</form>
+	</div>								
+</body>
+</html>
